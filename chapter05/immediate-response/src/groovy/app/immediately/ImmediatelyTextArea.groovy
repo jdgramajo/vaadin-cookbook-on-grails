8d09438b4c3f3c92667d3dfd9aabe24a883dcb73
Juan Diego Gramajo <juan.gramajo@xoom.com>
2015-05-27 17:44:56 -0600

    Adds recepie for immediate response to changes in a TextArea.

---
+++ chapter05/immediate-response/src/groovy/app/immediately/ImmediatelyTextArea.groovy
@@ -0,0 +1,41 @@
+package app.immediately
+
+import com.vaadin.event.FieldEvents.TextChangeListener
+import com.vaadin.ui.AbstractTextField.TextChangeEventMode
+import com.vaadin.ui.Button
+import com.vaadin.ui.Button.ClickListener
+import com.vaadin.ui.TextArea
+import com.vaadin.ui.VerticalLayout
+
+@SuppressWarnings("serial")
+public class ImmediatelyTextArea extends VerticalLayout{
+	
+	public ImmediatelyTextArea() {
+		TextArea textArea = new TextArea()
+		textArea.with {
+			setImmediate(true)
+			textArea.setTextChangeEventMode(TextChangeEventMode.EAGER)
+		}
+		
+		final Button saveButton = new Button("Saved")
+		saveButton.setEnabled(false)
+		
+		textArea.addTextChangeListener({ event ->
+			saveButton.with {
+				setCaption("Save")
+				setEnabled(true)
+			}
+		} as TextChangeListener)		
+		
+		saveButton.addClickListener({ event ->
+			saveButton.with {
+				setCaption("Saved")
+				setEnabled(false)
+			}
+		} as ClickListener)		
+		
+		addComponent(textArea)
+		addComponent(saveButton)		
+	}
+
+}
