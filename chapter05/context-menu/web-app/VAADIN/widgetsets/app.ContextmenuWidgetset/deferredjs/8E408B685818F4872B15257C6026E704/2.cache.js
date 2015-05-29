22a696a3c9e48eb2ca374eda3db6a1b6da4be6dc
Juan Diego Gramajo <juan.gramajo@xoom.com>
2015-05-29 16:25:34 -0600

    Third attempt, no luck

---
+++ chapter05/context-menu/web-app/VAADIN/widgetsets/app.ContextmenuWidgetset/deferredjs/8E408B685818F4872B15257C6026E704/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.app_ContextmenuWidgetset.runAsyncCallback2('Dab(1530,1,KLd);_.tc=function c9b(){gXb((!_Wb&&(_Wb=new lXb),_Wb),this.a.d)};pFd(Qh)(2);\n//# sourceURL=app.ContextmenuWidgetset-2.js\n')
