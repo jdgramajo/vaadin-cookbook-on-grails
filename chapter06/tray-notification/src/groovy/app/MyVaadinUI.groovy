ec58f15dffb17f8ae973e3ac9bfae2a210233c72
Juan Diego Gramajo <juan.gramajo@xoom.com>
2015-06-01 14:48:12 -0600

    Adds tray notification customization via CSS.
      - Learned that ClickListener may be implemented and passed to addClickListener as a class, but chose to do it by a closure, to take advantage of Groovy
      - Learned that only ONE styles.scss file may be added unther VAADIN/themes/${mytheme} or else it will not work

---
+++ chapter06/tray-notification/src/groovy/app/MyVaadinUI.groovy
@@ -0,0 +1,40 @@
+package app
+
+import com.vaadin.annotations.Theme
+import com.vaadin.server.Page
+import com.vaadin.server.VaadinRequest
+import com.vaadin.shared.Position
+import com.vaadin.ui.*
+import com.vaadin.ui.Button
+import com.vaadin.ui.Button.ClickEvent
+import com.vaadin.ui.Button.ClickListener
+import com.vaadin.ui.Notification
+import com.vaadin.ui.Notification.Type
+
+@Theme("mytheme")
+public class MyVaadinUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout()
+        layout.setMargin(true)
+        setContent(layout)
+
+        Button btn = new Button("Click me!")
+        btn.addClickListener({
+            Notification.show("Good job!", Type.TRAY_NOTIFICATION)
+
+            Notification notification = new Notification("Well done!", 
+                "You have clicked on the button.", Notification.Type.TRAY_NOTIFICATION)
+
+//            notification.setDelayMsec(0)
+            notification.setPosition(Position.TOP_RIGHT)
+            notification.setStyleName("mynotification")
+
+            notification.show(Page.getCurrent())
+        } as ClickListener)
+
+        layout.addComponent(btn)
+    }
+
+}
