e698b2a04e224b54fd6b1d20fdb9a6b40bb2e14d
Juan Diego Gramajo <juan.gramajo@xoom.com>
2015-06-02 14:13:46 -0600

    Adds upload progressbar recipe.

---
+++ chapter06/transfer-progressbar/src/groovy/app/MyVaadinUI.groovy
@@ -0,0 +1,81 @@
+package app
+
+import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.*
+import com.vaadin.ui.Button.ClickEvent
+
+/*import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream*/
+
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI {
+
+    private VerticalLayout layout = new VerticalLayout()
+
+    @Override
+    protected void init(VaadinRequest request) {
+        layout.setMargin(true)
+        setContent(layout)
+
+        ProgressIndicator indicator = new ProgressIndicator(0.0f)
+        UploadReceiver uploadReceiver = new UploadReceiver(indicator)
+        Upload upload = new Upload("Upload", uploadReceiver)
+        upload.with {
+            addProgressListener(uploadReceiver)
+            addFinishedListener(uploadReceiver)
+            addStartedListener(uploadReceiver)
+        }
+        layout.addComponent(upload)
+    }
+
+    class UploadReceiver implements Upload.Receiver, Upload.ProgressListener,
+            Upload.StartedListener, Upload.FinishedListener {
+
+        private final ProgressIndicator indicator
+
+        public UploadReceiver(ProgressIndicator indicator) {
+            this.indicator = indicator
+        }
+
+        @Override
+        public OutputStream receiveUpload(String filename, String mimeType) {
+            FileOutputStream fos = null
+            try {
+                File file = new File("/Users/jgramajo/Desktop/${filename}")
+                fos = new FileOutputStream(file)
+            } catch (FileNotFoundException e) {
+                Notification.show(e.getMessage(), Notification.Type.ERROR_MESSAGE)
+                return null
+            }
+            return fos
+        }
+
+        @Override
+        public void updateProgress(long readBytes, long contentLength) {
+            try {
+                // let's slow down upload a bit (so we can see the progress on
+                // local machine)
+                Thread.sleep(100)
+            } catch (InterruptedException e) {
+                e.printStackTrace()
+            }
+            float newValue = readBytes / contentLength as float
+            println newValue
+            indicator.setValue(newValue)
+        }
+
+        @Override
+        public void uploadStarted(Upload.StartedEvent event) {
+            layout.addComponent(indicator)
+            Notification.show("Upload started.", Notification.Type.TRAY_NOTIFICATION)
+        }
+
+        @Override
+        public void uploadFinished(Upload.FinishedEvent event) {
+            layout.removeComponent(indicator)
+            Notification.show("Upload finished.", Notification.Type.TRAY_NOTIFICATION)
+        }
+    }
+}
