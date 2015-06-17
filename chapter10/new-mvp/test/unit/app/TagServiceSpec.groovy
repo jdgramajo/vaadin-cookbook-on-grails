a38171d7f03f00f51a732e757fab4de1c1c2e8d0
diego <juan.gramajo@xoom.com>
2015-06-16 22:24:21 -0600

    Adds unit tests for TagService in TagServiceSpec, using Spock.
      - Since the Tag class IS used and referenced in TagService, it needs to be mocked with the @Mock annotation.

--- chapter10/new-mvp/test/unit/app/TagServiceSpec.groovy
+++ chapter10/new-mvp/test/unit/app/TagServiceSpec.groovy
@@ -7,14 +7,33 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(TagService)
+@Mock(Tag)
 class TagServiceSpec extends Specification {
 
-    def setup() {
+    void "Null collection when no Tag has been pushed"() {
+    	
+    	given: "No saves are made"
+    	
+    	when:
+    		def coll = service.getAllTags()
+    	
+    	then:
+    		!coll
+
     }
 
-    def cleanup() {
-    }
+    void "Collection<Tag> returned when Tags have been pushed"() {
 
-    void "test something"() {
+    	given: "Tags are saved"
+    		service.saveTag(new Tag(name: "one"))
+    		service.saveTag(new Tag(name: "two"))
+    		service.saveTag(new Tag(name: "three"))
+
+    	when: "Fetching tags"
+    		def coll = service.getAllTags()
+
+    	then: "Got a Collection<Tag>"
+    		coll instanceof Collection<Tag>
+
     }
 }
